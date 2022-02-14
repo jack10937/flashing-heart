f4154473211a47715af44b26f02ab18c96ede410
jack10937 <71825442+jack10937@users.noreply.github.com>
2022-02-14 09:16:07 +0800

    MakeCode 專案的初始檔案

---
+++ main.py
@@ -0,0 +1,24 @@
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+""")
+
+def on_forever():
+    basic.show_leds("""
+        . # . # .
+                # # # # #
+                # # # # #
+                . # # # .
+                . . # . .
+    """)
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+    """)
+basic.forever(on_forever)
